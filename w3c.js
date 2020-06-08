643ee955879e15826a1f0d4e31bd1eaef7a68a13
plehegar <plh@w3.org>
2020-06-08 18:34:37 -0400

    Added datatracker

--- w3c.js
+++ w3c.js
@@ -113,6 +113,10 @@ async function ongroup(group) {
   // Some additional useful links
   group["details"] = `https://www.w3.org/2000/09/dbwg/details?group=${groupId}&order=org&public=1`;
 
+  const datatracker_file = 'shortnames.json';
+  group["datatracker"] = new LazyPromise(() => fetchJSON(datatracker_file)
+     .then(data => data.find(g => (g.id === group.id))));
+
   // the dashboard knows about spec milestones and a subset of GH repositories issues
   group["dashboard"] = {
     href: `https://w3c.github.io/spec-dashboard/?${groupId}`,
