2d786ecdd72bafe52b35f74d6ce4844f86940b5b
plehegar <plh@w3.org>
2021-02-19 12:59:47 -0500

    Use promises. Add functions used by boards

--- start.js
+++ start.js
@@ -17,26 +17,49 @@ class DOMParser {
 global.DOMParser = DOMParser;
 global.fetch = fetch;
 
-function renderGroup(url, cb) {
-  JSDOM.fromURL(url).then(dom => {
-    const document = dom.window.document;
+const functions =
+{
+  filterOpenIssues: function (issues) {
+    let openIssues = issues.filter(i => i.state === "open" && !i.pull_request);
+    return { length: openIssues.length };
+  },
+  filterOpenPRs: function (issues) {
+    let openPRs = issues.filter(i => i.state === "open" && i.pull_request);
+    return { length: openPRs.length };
+  },
+  activeSpecs: async function (specs) {
+    return (await Promise.all(specs.map(spec =>
+      spec["latest-status"].then(s => (s !== "Retired") ? spec : undefined)))).filter(spec => spec);
+  },
+  milestones: function (milestones) {
+    return Object.entries(milestones).map(s => s[0] + " " + s[1]);
+  },
+  mlConfig: function (confs) {
+    return Object.keys(confs).map(c => " " + c);
+  },
+};
 
-    function save() {
-      let scripts = document.querySelectorAll("script");
-      for (let script of scripts) script.parentNode.removeChild(script);
-      cb(dom.serialize());
-    }
-    setW3CKey(document.documentElement.getAttribute("data-apiary-key"));
-    subscribe("done", save);
+function renderGroup(url) {
+  return new Promise((resolve, reject) => {
+    JSDOM.fromURL(url).then(dom => {
+      const document = dom.window.document;
 
-    let gid = document.documentElement.getAttribute("data-gid");
+      function done() {
+        let scripts = document.querySelectorAll("script");
+        for (let script of scripts) script.parentNode.removeChild(script);
+        resolve(dom.serialize());
+      }
+      setW3CKey(document.documentElement.getAttribute("data-apiary-key"));
+      subscribe("done", done);
 
+      let gid = document.documentElement.getAttribute("data-gid");
 
-    fetchGroup(gid).then(group => {
-      let elts = document.querySelectorAll("*.group-data");
-      for (let element of elts) jsonrender({ group: group }, element);
-    });
-  }).catch(console.error);
+      fetchGroup(gid).then(group => {
+        let elts = document.querySelectorAll("*.group-data");
+        for (let element of elts) jsonrender({ group: group }, element, functions);
+      });
+    }).catch(reject);
+  });
 }
 
 
