83ae47f2bcfb726fb24b278593336fab479b24f7
plehegar <plh@w3.org>
2024-09-12 13:27:52 -0400

    Changed shorting order for events

--- w3cevents.js
+++ w3cevents.js
@@ -2,16 +2,16 @@ import loadICS from "./ics.js";
 
 let EVENT_CACHE = [];
 function sortEvents(a, b) {
-  // first rec track, then note, then alphabetical order
+  // sort by most recent first
   function criteria(v) {
     return (v.dtstart) ? v.dtstart : "";
   }
   let vA = criteria(a);
   let vB = criteria(b);
-  if (vA < vB) {
+  if (vA > vB) {
     return -1;
   }
-  if (vA > vB) {
+  if (vA < vB) {
     return 1;
   }
 
